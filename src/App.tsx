acdfb0b0be30a1f862e8f640fa28bb09d5dbfbb0
Samoo1234 <samoelduartelacerda@gmail.com>
2026-04-27 11:15:42 -0300

    fix: remove unused React import in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { BrowserRouter, Routes, Route, Link, Navigate } from 'react-router-dom';
 import { Recepcao } from './pages/Recepcao';
 import { Pulpito } from './pages/Pulpito';
